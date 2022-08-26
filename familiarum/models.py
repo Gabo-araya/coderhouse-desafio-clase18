4f085235408800158b634b5ec34afda9023b7093
Gabo Araya <gabo.araya@gmail.com>
2022-08-25 21:17:06 -0400

    agrega imagenes + revisión de errores + correcciones menores

--- familiarum/models.py
+++ familiarum/models.py
@@ -6,12 +6,13 @@ from django.db import models
 
 class Persona(models.Model):
     ''' Este modelo guarda los datos de las personas. '''
-    nombre_completo = models.CharField(max_length=250, null=True, blank=True, default='', verbose_name='Nombre completo')
+    nombre_completo = models.CharField(max_length=250, verbose_name='Nombre completo')
     # slug = models.SlugField(null=True, blank=True, verbose_name='Slug')
-    fecha_nacimiento = models.DateField(auto_now_add=True, null=True, blank=True, verbose_name='Fecha de nacimiento')
+    fecha_nacimiento = models.DateField(verbose_name='Fecha de nacimiento')
     cedula_identidad = models.IntegerField(null=True, blank=True, verbose_name='Cédula Identidad')
     direccion = models.TextField(null=True, blank=True, verbose_name='Dirección')
-    observaciones = models.TextField(null=True, blank=True, verbose_name='Observaciones')
+    observaciones = models.TextField(null=True, blank=True, verbose_name='Biografía')
+    imagen =  models.CharField(max_length=250, null=True, blank=True, default='', verbose_name='Imagen')
     
     # campos de gestión
     #activo = models.BooleanField(null=True, blank=True, default=True, verbose_name='Activo')
@@ -20,9 +21,7 @@ class Persona(models.Model):
     #deleted = models.DateTimeField(null=True, blank=True, verbose_name='Fecha de desactivación')
 
     class Meta:
-        ''' class Meta:
-            Define el nombre singular y plural, y el ordenamiento de los elementos
-        '''
+        ''' class Meta: Define el nombre singular y plural, y el ordenamiento de los elementos. '''
         verbose_name = 'Persona'
         verbose_name_plural = 'Personas'
         ordering = ['id']
